a1e1cbc33211772ace03657246e4e698920a79ad
Ezra Skwarka <ezraskwarka@gmail.com>
2022-10-24 09:25:55 -0400

    Capture Room input

--- README.md
+++ README.md
@@ -8,6 +8,10 @@
 
 
 
-Usefull links:
+### Usefull links:
 
+Local Python Server:
+https://developer.mozilla.org/en-US/docs/Learn/Common_questions/set_up_a_local_testing_server
+
+Understanding Fetch:
 https://howtocreateapps.com/fetch-and-display-json-html-javascript/
---
+++ Scripts/roomReader.js
@@ -0,0 +1,57 @@
+function requestRoom(ID_target) {
+	fetch("Assets/small_example_adventure.json")
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                appendData(data);
+            })
+            .catch(function (err) {
+                console.log('error: ' + err);
+            });
+        function appendData(data) {
+            var mainContainer = document.getElementById("demo");
+            for (var i = 0; i < data.length; i++) {
+				if(data[i].ID == ID_target) {
+					//Create new container
+					var div = document.createElement("div");
+					//Formet Text
+					div.innerHTML = 'Name: ' + data[i].short_name;
+					//Append to Parent
+					mainContainer.appendChild(div);
+				}
+            }
+        }
+}
+
+function requestRoomRange(ID_target_min, ID_target_max) {
+	fetch("Assets/small_example_adventure.json")
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                appendData(data);
+            })
+            .catch(function (err) {
+                console.log('error: ' + err);
+            });
+        function appendData(data) {
+            var mainContainer = document.getElementById("demo");
+            for (var i = 0; i < data.length; i++) {
+				if(and((data[i].ID >= ID_target_min),(data[i].ID <= ID_target_max)) ) {
+					//Create new container
+					var div = document.createElement("div");
+					//Formet Text
+					div.innerHTML = 'Name: ' + data[i].short_name;
+					//Append to Parent
+					mainContainer.appendChild(div);
+				}
+            }
+        }
+}
+
+
+function readRoomInput (x) {
+	requestRoom(x);
+	//will need to make sure room ID 'x' exists and if not output something to the user so they know why it failed
+}
