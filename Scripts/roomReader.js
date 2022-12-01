4195aff72612da8b6d8580e871cf21dec6bb5ade
Ezra Skwarka <ezraskwarka@gmail.com>
2022-12-01 13:13:45 -0500

    move to better typewriting effect

--- Scripts/roomReader.js
+++ Scripts/roomReader.js
@@ -13,6 +13,7 @@ function requestRoom(ID_target) {
             var mainContainer = document.getElementById("gameText");
             for (var i = 0; i < data.length; i++) {
 				if(data[i].ID == ID_target) {
+					// console.log("Typing Room " + String(data[i]) + "in element " + String(mainContainer))
 					typeRoom(data[i], mainContainer)
 				}
             }
@@ -47,6 +48,7 @@ function requestRoomRange(ID_target_min, ID_target_max) {
 
 
 function readRoomInput (x) {
+	console.log("Room Input: " + String(x))
 	requestRoom(x);
 	//will need to make sure room ID 'x' exists and if not output something to the user so they know why it failed
 }
@@ -55,41 +57,47 @@ function printRoom(roomData, mainContainer) {
 	//Create new container
 	var div = document.createElement("div");
 	//Formet Text
-	div.innerHTML = (
+	div.innerHTML = String((
 		roomData.ID + '   ' + roomData.short_name + '<br><br>' +
 		roomData.entry_text + '<br><br>' +
 		roomData.exit_text + '<br><br>'
 		
 		
 		
-		);
+		));
 	//Append to Parent
 	mainContainer.appendChild(div);
 	
 }
 
 function typeRoom(roomData, mainContainer) {
-
-	var customNodeCreator = function(character) {
-		return document.createTextNode(character);
-	}
+	
+	//Create new container
 	var div = document.createElement("div");
 	mainContainer.appendChild(div);
+	// console.log("Child: " + String(div))
 
-	var typewriter = new Typewriter(div, {
-	loop: false,
-	delay: 25,
-	onCreateTextNode: customNodeCreator,
-	});
 
+	//Formet Text
 	var textString = String((
 		roomData.ID + ' - ' + roomData.short_name + '<br><br>' +
 		roomData.entry_text + '<br><br>' +
 		roomData.exit_text
 		));
-	typewriter
-	.typeString(textString)
-	.typeString('test')
-	.pauseFor(300)
-	.start();
+	// console.log("textString: " + textString)
+	
+	//Append to Parent
+	appearChars(textString, div, 20)
+	
+}
+
+/** make text appear in the innerHTML of the element, one character at a time per timeBetween via: Unknown, credit needed*/
+function appearChars(str, elem, timeBetween) {
+    var index = -1;
+    (function go() {
+        if (++index < str.length) {
+         	elem.innerHTML = elem.innerHTML + str.charAt(index);
+            setTimeout(go, timeBetween);
+        }
+    })();
 }
